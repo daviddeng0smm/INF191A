e9e5c26cd9674d1d315844385ba17a4f1f07d39d
daviddeng0smm <daviddeng0smm@gmail.com>
2026-03-27 22:15:50 -0700

    no changes made, git is just being sensitive

--- Backend/src/main/java/com/pm/backend/BackendApplication.java
+++ Backend/src/main/java/com/pm/backend/BackendApplication.java
@@ -37,8 +37,8 @@ public class BackendApplication implements CommandLineRunner {
 
         // This call now returns INSTANTLY because of @Async.
         // The main thread doesn't wait for the loop to finish!
-        historicalStreamer.StartHistoricalStreamer(new String[]{"DAL371", "AAL125"}, 1774270800, 1774272600);
-
+//        historicalStreamer.StartHistoricalStreamer(new String[]{"DAL371", "AAL125"}, 1774270800, 1774272600);
+        liveStreamer.startLiveStreaming("KLAX");
         System.out.println("Main thread is free to handle WebSockets!");
     }
 }
